0f18f52fb81fd4d0bbfe5c8c527bdb31ece73dee
Bodeu97 <lukaszebo@gmail.com>
2021-04-14 17:46:03 +0200

    upd

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import numpy as np
 
-# Zad 1
+# #Zad 1
 # a = 4*np.arange(20)
 # print(a)
-# Zad 2
+# # Zad 2
 # a = np.arange(10, dtype='float')
 # print(a)
 # b = a.astype('int32')
 # print(b)
-# Zad 3
+# #Zad 3
 # def powerOf2Array(n : int):
 #     a = 2**np.mgrid[1:n+1, 1:n+1]
 #     return a
 #
 # print(powerOf2Array(20))
-#Zad 4
-
+#
+# #Zad 4
+# def tablicaPotegowa(n, m):
+#     a = np.logspace(start=1,num=m, stop=m, base=n, dtype='int')
+#     return a
+# print(tablicaPotegowa(2, 8))
+#
+#
